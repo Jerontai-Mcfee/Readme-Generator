acb611049632e28cdb9f4ab628ce794887774b74
GuiltyGamingJay <jerontai07@gmail.com>
2022-10-27 23:29:47 -0400

    variables written in index,  questions started

---
+++ Assets/index.js
@@ -0,0 +1,13 @@
+var fileGenerator = require("./fileGenerator");
+var fs = require("fs");
+var inquirer = require('inquirer');
+
+//Questions//
+
+let questions=[{
+
+type: "input",
+message:"what is the title of your repository?",
+name: "title",
+    
+}]
