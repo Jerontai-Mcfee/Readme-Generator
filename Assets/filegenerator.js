f16c175de966ba73429d7a02a47063f9b91988c3
GuiltyGamingJay <jerontai07@gmail.com>
2022-11-03 04:59:05 -0400

    filegen and index final edits

--- Assets/filegenerator.js
+++ Assets/filegenerator.js
@@ -1,6 +1,8 @@
-function fileGenerator(response) {
+function fileGenerator(response) 
+{
     var content = `# ${response.title}
-##### ${response.logo}[${response.userName}](${response.GitHub})[Email Me](${response.email}) 
+##### [${response.userName}](${response.GitHub})[Email Me @](${response.email}) 
+#### Readme generator [License used]  ${response.license} 
 
 ## Table of Contents
     * [Repository Description](#Repository-Description)
@@ -12,44 +14,43 @@ function fileGenerator(response) {
 ###### [Back to Table of Contents](#Table-of-Contents)
 ${response.description}
 
+###### [Back to Table of Contents](#Table-of-Contents)
 ## User Story
-### As the Quiz Administrator
-###### [Back to Table of Contents](#Table-of-Contents)
-1.   I a command-line application that accepts user input.
-2.   I want to be prompted for information about my application repository.
-3.   I want a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contribution Guidelines, Tests Instructions, and Questions.
-4.   I want my project title to display as the title of the README.
-5.   I want a description, installation instructions, usage information, contribution guidelines, and test instructions; and then, this information is added to the sections of the README entitled Description, Installation, Usage, Contribution Guidelines, and Tests Instructions.
-6.   I want to choose a license for my application from a list of options; and then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.  
-7.   I want to enter my GitHub username; ant then this is added to the section of the README entitled Questions, with a link to my GitHub profile.
-8.   I want to enter my email address; and then this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions.
-9.   I want to click on the links in the Table of Contents; and then, I am taken to the corresponding section of the README.
 
-###### [Back to Table of Contents](#Table-of-Contents)
-${response.contribute}
+1.${response.userstory1}
+2.${response.userstory2}
+3.${response.userstory3}
+4.${response.userstory4}
+5.${response.userstory5}
+6.${response.userstory6}
+7.${response.userstory7}
+8.${response.userstory8}
+9.${response.userstory9}
 
-## Test Instructions
+
+##  Instructions
 ###### [Back to Table of Contents](#Table-of-Contents)
-1.  ${response.test}
-2.  ${response.test2}
-3.  ${response.test3}
+1.  ${response.contents1}
+2.  ${response.contents2}
+3.  ${response.contents3}
 
 ## Overview: What We Accomplished!
 ### Workflow [Back to Table of Contents](#Table-of-Contents)
-${response.accomplish}
+${response.accomplishments}
 
-#### Demo I createad ![fileGenerator.js](${response.fileGnerator})
+#### Demo I created ![fileGenerator.js](${response.fileGnerator})
     
-#### Demo I createad ![ReadMe.md](${response.ReadMe})
+#### Demo I created ![ReadMe.md](${response.ReadMe})
 
 ## Repositiory Goals Criterea
 ###### [Back to Table of Contents](#Table-of-Contents)
-${response.goals}
+${response.Goals}
 
 ## License
     ${response.license}
-    Please feel free to fork and even collaborate with me on this awesome [repository](https://github.com/supercodingninja/CodeQuiz ).  Thank you for viewing! - Frederick Thomas, Super Coding Ninja™ [2014-Present](https://supercodingninja.github.io/) All Rights Reserved
     
+    Copyright (c) [2022] [Je'Rontai Mcfee]
+
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
     
     The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -58,5 +59,3 @@ ${response.goals}
     
     return content;
 };
-
-module.exports = fileGenerator;
--- Assets/index.js
+++ Assets/index.js
@@ -4,7 +4,7 @@ var inquirer = require('inquirer');
 
 //Questions//
 
-let questions=[
+ const questions=[
     {
 
     type: "input",
@@ -14,7 +14,7 @@ let questions=[
 },{
     type: "input",
     message: "what is your Github user name? ",
-    name: "username"
+    name: "userName"
 },{
     type: "input",
     message: "Please give your GitHub profile link.",
@@ -36,12 +36,61 @@ let questions=[
     ],
 },{
     type: "input",
+    message: "instructions 1 (1/3).",
+    name: "contents1",
+},{
+    type: "input",
+    message: "instructions 2 (2/3).",
+    name: "contents2",
+},{
+    type: "input",
+    message: "instructions 3 (3/3).",
+    name: "contents3",
+},{
+    type: "input",
+    message: "user story 1 (1/9).",
+    name: "userstory1",
+},{
+    type: "input",
+    message: "user story 2 (2/9).",
+    name: "userstory 2",
+},{
+    type: "input",
+    message: "user story 3 (3/9).",
+    name: "userstory3",
+},{
+    type: "input",
+    message: "user story 4 (4/9).",
+    name: "userstory4",
+},{
+    type: "input",
+    message: "user story 5 (5/9).",
+    name: "userstory5",
+},{
+    type: "input",
+    message: "user story 6 (6/9).",
+    name: "userstory6",
+},{
+    type: "input",
+    message: "user story 7 (7/9).",
+    name: "userstory7",
+},{
+    type: "input",
+    message: "user story 8 (8/9).",
+    name: "userstory8",
+},{
+    type: "input",
+    message: "user story 9 (9/9).",
+    name: "userstory9"
+},{
+    
+    type: "input",
     message: "Please describe the repository.",
     name: "description"
 },{
     type: "input",
     message: "State your accomplishments.",
-    name: "accomplish"
+    name: "accomplishments",
 },{
     type: "input",
         message: "Please state your goals.",
