a50182f97c0c67bbcc17f27c4b8f6d39e5a392bf
GuiltyGamingJay <jerontai07@gmail.com>
2022-11-03 00:22:58 -0400

    finishing code for turn in

---
+++ Assets/filegenerator.js
@@ -0,0 +1,62 @@
+function fileGenerator(response) {
+    var content = `# ${response.title}
+##### ${response.logo}[${response.userName}](${response.GitHub})[Email Me](${response.email}) 
+
+## Table of Contents
+    * [Repository Description](#Repository-Description)
+    * [Installation Instructions](#Install)
+    * [Usage Information](#Usage)
+    * [Repositiory Goals Criterea](#Criterea)
+ 
+# Repository Description
+###### [Back to Table of Contents](#Table-of-Contents)
+${response.description}
+
+## User Story
+### As the Quiz Administrator
+###### [Back to Table of Contents](#Table-of-Contents)
+1.   I a command-line application that accepts user input.
+2.   I want to be prompted for information about my application repository.
+3.   I want a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contribution Guidelines, Tests Instructions, and Questions.
+4.   I want my project title to display as the title of the README.
+5.   I want a description, installation instructions, usage information, contribution guidelines, and test instructions; and then, this information is added to the sections of the README entitled Description, Installation, Usage, Contribution Guidelines, and Tests Instructions.
+6.   I want to choose a license for my application from a list of options; and then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under.  
+7.   I want to enter my GitHub username; ant then this is added to the section of the README entitled Questions, with a link to my GitHub profile.
+8.   I want to enter my email address; and then this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions.
+9.   I want to click on the links in the Table of Contents; and then, I am taken to the corresponding section of the README.
+
+###### [Back to Table of Contents](#Table-of-Contents)
+${response.contribute}
+
+## Test Instructions
+###### [Back to Table of Contents](#Table-of-Contents)
+1.  ${response.test}
+2.  ${response.test2}
+3.  ${response.test3}
+
+## Overview: What We Accomplished!
+### Workflow [Back to Table of Contents](#Table-of-Contents)
+${response.accomplish}
+
+#### Demo I createad ![fileGenerator.js](${response.fileGnerator})
+    
+#### Demo I createad ![ReadMe.md](${response.ReadMe})
+
+## Repositiory Goals Criterea
+###### [Back to Table of Contents](#Table-of-Contents)
+${response.goals}
+
+## License
+    ${response.license}
+    Please feel free to fork and even collaborate with me on this awesome [repository](https://github.com/supercodingninja/CodeQuiz ).  Thank you for viewing! - Frederick Thomas, Super Coding Ninja™ [2014-Present](https://supercodingninja.github.io/) All Rights Reserved
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+    
+    return content;
+};
+
+module.exports = fileGenerator;
--- Assets/index.js
+++ Assets/index.js
@@ -1,10 +1,11 @@
-var fileGenerator = require("./fileGenerator");
+var fileGenerator = require("./filegenerator");
 var fs = require("fs");
 var inquirer = require('inquirer');
 
 //Questions//
 
-const questions=[{
+let questions=[
+    {
 
     type: "input",
     message:"what is the title of your repository?",
@@ -12,6 +13,50 @@ const questions=[{
 
 },{
     type: "input",
-    message: "please "
+    message: "what is your Github user name? ",
+    name: "username"
+},{
+    type: "input",
+    message: "Please give your GitHub profile link.",
+    name: "GitHub"
+},{
+    type: "input",
+    message: "What is your email?",
+    name: "email"
+},{
+    type:"list",
+    name:"license",
+    message:"Please select which license you would like to use.",
+    choices : [
+        "APACHE 2.O",
+        "BSD 3",
+        "GVL-GPL 3.0",
+        "MIT",
+        "None"
+    ],
+},{
+    type: "input",
+    message: "Please describe the repository.",
+    name: "description"
+},{
+    type: "input",
+    message: "State your accomplishments.",
+    name: "accomplish"
+},{
+    type: "input",
+        message: "Please state your goals.",
+        name: "Goals"
+}
+];
 
-}];
+// Function to write to my ReadMe.md file. //
+inquirer.prompt(questions).then(function(response) {
+    console.log(response);
+    
+     var content = fileGenerator(response);
+     console.log(content);
+      fs.writeFile("./ReadMe.md", content, function(err){
+          if (err) throw err
+          console.log("success");
+      });
+ } ); 
